b18f2bc1f98ef72d70518f8fba8cd7660c8e5564
Madina <madicorit@gmail.com>
2023-03-11 18:21:53 +0300

    HW 1_1

---
+++ Lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.WriteLine ("Enter the first number: ");
+int n_1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ("Enter the second number: ");
+int n_2 = int.Parse(Console.ReadLine()!);
+
+if (n_1 > n_2)
+{
+  Console.Write($"The number {n_1} is greater than {n_2}");
+}
+else if (n_1 == n_2)
+{
+  Console.Write("$The number {n_1} IServiceProvider equal to the number {n_2}");
+}
+else
+{
+  Console.Write($"The number {n_2} is greater than {n_1}");
+}
