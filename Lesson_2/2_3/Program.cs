880f281a3561416ce4a6be2b8dbde43ac5ceffd8
Madina <madicorit@gmail.com>
2023-03-12 19:03:56 +0300

    Task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+int a = new Random().Next(100, 1000);
+
+void MyFunc(int num)
+{
+  Console.WriteLine(num);
+  if(num % 7 == 0 && num % 23 == 0)
+  {
+    Console.WriteLine("Кратно");
+  }
+  else
+  {
+    Console.WriteLine("Не кратно");
+  }
+}
+
+MyFunc(a);
