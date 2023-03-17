fee46dc5080f163d50aa21d47d24bf7f1837cb19
Madina <madicorit@gmail.com>
2023-03-17 19:41:00 +0300

    HW Task 3_3

---
+++ Lesson_3/HW/3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Please, enter a number: ");
+void Pow(int num)
+{
+  int i = 1;
+  while (i <= num)
+  {
+    Console.WriteLine(Math.Pow(i, 3));
+    i++;
+  }
+}
+
+int number = int.Parse(Console.ReadLine()!);
+Pow(number);
