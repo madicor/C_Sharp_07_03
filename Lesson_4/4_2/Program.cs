e976f408dca11d43729726a3bd721fb6f9999996
Madina <madicorit@gmail.com>
2023-03-18 15:55:29 +0300

    Task 4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int num = GetUserNumber();
+int digits = MultiplyNumbers(num);
+Console.WriteLine(digits);
+
+int GetUserNumber()
+{
+  int number = 0;
+  while(number == 0)
+  {
+    Console.Write("Please, enter a number: ");
+    number = int.Parse(Console.ReadLine()!);
+  }
+  return number;
+}
+
+int MultiplyNumbers(int n)
+{
+  int multi = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    multi = multi * i;
+  }
+  return multi;
+}
