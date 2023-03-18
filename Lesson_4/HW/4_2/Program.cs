276004ab7cbffb00b0e87964fda815482238faa5
Madina <madicorit@gmail.com>
2023-03-18 16:25:46 +0300

    HW Task 4_2

---
+++ Lesson_4/HW/4_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int num = GetUserNumber();
+int digits = SumNumbers(num);
+Console.WriteLine(digits);
+
+int GetUserNumber()
+{
+  int number = 0;
+  while(number == 0)
+  {
+    Console.Write("Please, enter a number: ");
+    number = int.Parse(Console.ReadLine()!);
+  }
+  return number;
+}
+
+int SumNumbers(int n)
+{
+  int sum = 0;
+  for (int i = 1; i <= n; i++)
+  {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
