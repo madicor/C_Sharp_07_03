ea7871e577d6c2b8fffb744ca3844e539d57e2a0
Madina <madicorit@gmail.com>
2023-03-20 17:45:46 +0300

    HW Task 4_3

---
+++ Lesson_4/HW/4_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов случайными числами 
+// и выводит их на экран. Оформите заполнение массива и вывод в виде функции
+// (пригодится в следующих задачах)
+// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
+// 6,1,33,5,6,7,7,1 -> 6,1,33,5,6,7,7,1
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
