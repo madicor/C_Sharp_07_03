8c2438f505f455f5f684aab2528afbe23ae86961
Madina <madicorit@gmail.com>
2023-04-06 23:08:44 +0300

    HW Task 8_2

---
+++ Lesson_8/HW/8_2/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов.
+
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[,] Gen2DArrayInt(int row, int column, int range)
+{
+  int i = 0; int j = 0;
+  int[,] arr = new int[row, column];
+  while (i < row)
+  {
+    j = 0;
+    while (j < column)
+    {
+      arr[i, j] = new Random().Next(0, range);
+      j++;
+    }
+    i++;
+  }
+  return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine("");
+  }
+}
+
+int SumLine(int[,] array, int i)
+{
+  int sumLine = array[i, 0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i, j];
+  }
+  return sumLine;
+}
+
+Console.Clear();
+
+int row = ReadData("Количество строк матрицы: ");
+int column = ReadData("Количество столбцов матрицы: ");
+int range = ReadData("Диапазон элементов массива от 0 до ..... ? : ");
+
+int[,] arr2D = Gen2DArrayInt(row, column, range);
+
+Console.WriteLine("Исходный 2D массив: ");
+Print2DArr(arr2D);
+
+int minSumLine = 0;
+int sumLine = SumLine(arr2D, 0);
+for (int i = 1; i < arr2D.GetLength(0); i++)
+{
+  int tempSumLine = SumLine(arr2D, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+minSumLine = minSumLine + 1;
+
+Console.WriteLine();
+Console.WriteLine("Строка с наименьшей суммой равна: " + minSumLine);
+Console.WriteLine("Наименьшая сумма равна: " + sumLine);
