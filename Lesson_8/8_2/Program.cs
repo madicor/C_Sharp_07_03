ec764894f1db45b44b4a53e5f8d77fd169904f56
Madina <madicorit@gmail.com>
2023-04-06 22:54:27 +0300

    Task 8_2

---
+++ Lesson_8/8_2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int arrRows = 3;
+int arrColumns = 3;
+int arrMin = 1;
+int arrMax = 9;
+
+int[,] arr = Make2DArray(arrRows, arrColumns, arrMin, arrMax);
+Print2DArray(arr);
+int[] freq = CountFrequency(arr, arrMax);
+PrintFrequencies(freq);
+
+int[] CountFrequency(int[,] array, int max)
+{
+  int rows = array.GetLength(0);
+  int columns = array.GetLength(1);
+
+  int[] frequencies = new int[max + 1];
+
+  foreach (int elem in array)
+  {
+    frequencies[elem]++;
+  }
+
+  return frequencies;
+}
+
+void PrintFrequencies(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.WriteLine($"Количество {i} -> {array[i]}");
+  }
+  Console.WriteLine();
+}
+
+int[,] Make2DArray(int rows, int columns, int min, int max)
+{
+  int[,] arr = new int[rows, columns];
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      arr[i, j] = new Random().Next(min, max + 1);
+    }
+  }
+
+  return arr;
+}
+
+void Print2DArray(int[,] array)
+{
+  int rows = array.GetUpperBound(0) + 1;
+  int columns = array.Length / rows;
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+  Console.WriteLine();
+}
