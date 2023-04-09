271f8391245527de6a4c55ebe79de2a2eff8cc3a
Madina <madicorit@gmail.com>
2023-04-09 18:20:08 +0300

    HW Task 9_2

---
+++ Lesson_9/HW/9_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int start = GetUserNumber("begin number");
+int finish = GetUserNumber("end number");
+Console.WriteLine(SumNumbers(start, finish));
+
+int GetUserNumber(string name)
+{
+  int number = 0;
+  Console.Write($"Please enter {name}: ");
+  number = int.Parse(Console.ReadLine()!);
+  return number;
+}
+
+int SumNumbers(int begin, int end)
+{
+  if (end < begin + 1)
+  {
+    return end;
+  }
+  return SumNumbers(begin, end - 1) + end;
+}
