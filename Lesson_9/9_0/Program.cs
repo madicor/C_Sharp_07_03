504578cbab77621a74f552bb0517ed6fe25f364e
Madina <madicorit@gmail.com>
2023-04-09 13:28:32 +0300

    Task 9_0

---
+++ Lesson_9/9_0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+//input data
+Console.Write("Enter the first number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+//functions
+void PrintNums(int n)
+{
+  if (n == 0) return;
+  PrintNums(n - 1);
+  Console.Write($"{n} ");
+}
+
+//result
+Console.Write("Result: ");
+PrintNums(N);
