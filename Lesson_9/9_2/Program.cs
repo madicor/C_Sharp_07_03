2490b7f05762776a6f5c43e9021ad386e1de6882
Madina <madicorit@gmail.com>
2023-04-09 18:01:16 +0300

    Task 9_2

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int num = GetUserNumber("number");
+int sum = 0;
+SumNumbers(num);
+Console.WriteLine(sum);
+
+int GetUserNumber(string name)
+{
+  int number = 0;
+  Console.Write($"Please enter {name}: ");
+  number = int.Parse(Console.ReadLine()!);
+  return number;
+}
+
+void SumNumbers(int number)
+{
+  if (number % 10 == 0)
+  {
+    return;
+  }
+  SumNumbers(number / 10);
+  sum += number % 10;
+}
